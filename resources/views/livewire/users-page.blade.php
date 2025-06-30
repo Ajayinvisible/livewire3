83b77d6e67ff4bcee884660ec65c7fe7a893b14c
Ajay Rai <rai.invisibleweb@gmail.com>
2025-06-30 20:36:22 +0545

    feat: dynamic user

--- resources/views/livewire/users-page.blade.php
+++ resources/views/livewire/users-page.blade.php
@@ -1,3 +1,3 @@
 <div class="max-w-screen-xl mx-auto p-4">
-    <h1 class="text-3xl text-slate-800">Hello From User Page</h1>
+    <h1 class="text-3xl text-slate-800">Hello From User {{ $user->name }}</h1>
 </div>
